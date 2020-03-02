6824ac94fe808d16a3540c7a3f41e20c7ecc53b0
Armand Stanczak <armandstanczak@gmail.com>
2020-03-02 20:12:35 +0100

    Array Tasks

---
+++ src/arraytasks/Task001_ArraySort.java
@@ -0,0 +1,16 @@
+package arraytasks;
+
+/**
+ *
+ * @author Armand Stańczak
+ */
+public class Task001_ArraySort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
