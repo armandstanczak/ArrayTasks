929dae2d653e23cad9b092eca49ab345f1b2e4ad
Armand Stanczak <armandstanczak@gmail.com>
2020-03-15 20:49:42 +0100

    Array Tasks

---
+++ src/arraytasks/Task019_AddMatrix.java
@@ -0,0 +1,24 @@
+package arraytasks;
+
+public class Task019_AddMatrix {
+
+    public static void main(String[] args) {
+        
+        int[][] arr1 = {{1, 7}, {1, 1}};
+        int[][] arr2 = {{2, 2}, {2, 2}};
+        int[][] arr3 = new int[arr1.length][arr1[0].length];
+        
+        for (int i = 0; i <= arr1.length - 1; i++) {
+            for (int j = 0; j <= arr2.length - 1; j++) {
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
+        for (int i = 0; i <= arr3.length - 1; i++) {
+            for (int j = 0; j<= arr3[0].length - 1; j++) {
+                System.out.print(arr3[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
+}
